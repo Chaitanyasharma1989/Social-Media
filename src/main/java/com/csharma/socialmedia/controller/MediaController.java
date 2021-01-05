8d843b18d7b15dada0dd35f4a887dc9d63dce144
Chaitanya Sharma <chaitanyasharma1989@gmail.com>
2021-01-05 18:59:28 +0530

    @csharma-Optimised imports

--- src/main/java/com/csharma/socialmedia/controller/MediaController.java
+++ src/main/java/com/csharma/socialmedia/controller/MediaController.java
@@ -8,7 +8,6 @@ import com.csharma.socialmedia.service.mediaservice.MediaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
